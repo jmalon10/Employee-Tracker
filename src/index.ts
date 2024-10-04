1a1ebf9251d44f547de0943f2a1fcecb91daf463
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-04 11:27:44 -0400

    adds functionality to add a department

--- src/index.ts
+++ src/index.ts
@@ -10,47 +10,101 @@ class Queries {
     constructor(pool: Pool) {
                 this.pool = pool; 
             }
-    async performActions(): Promise<void> {
-    const answers = await inquirer.prompt([       
-        {
-          type: 'list',
-          name: 'action',
-          message: 'What Would You Like to Do?',
-          choices: [
-            'View All Departments',
-            'View All Roles',
-            'View All Employees',
-            'Add a Department',
-            'Add a Role',
-            'Add an Employee',
-            'Update an Employee Role',
-            'Exit',
-          ],
-        },
-      ])
-      .then((answers: any) => {
-        if (answers.action === 'View All Departments') {
-            this.viewAllDepartments();
-        } else if (answers.action === 'View All Roles') {
-             this.viewAllRoles();
-        }else if (answers.action === 'View All Employees') {
-             this.viewAllEmployees();
-        }else if (answers.action === 'Add a Department') {
-             this.addDepartment();
-        }else if (answers.action === 'Add a Role') {
-             this.addRole();
-        }else if (answers.action === 'Add an Employee') {
-             this.addEmployee();
-        }else if (answers.action === 'Update an Employee Role') {
-             this.updateEmployeeRole();
-        }else if (answers.action === 'Exit') {
-            this.exit = true;
-            process.exit(0);
-        }if (!this.exit) {
-             this.performActions(); // Recursively call to show the menu again
-        }
-      })
-    }
+            async performActions(): Promise<void> {
+                const answers = await inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'action',
+                        message: 'What Would You Like to Do?',
+                        choices: [
+                            'View All Departments',
+                            'View All Roles',
+                            'View All Employees',
+                            'Add a Department',
+                            'Add a Role',
+                            'Add an Employee',
+                            'Update an Employee Role',
+                            'Exit',
+                        ],
+                    },
+                ]);
+            
+                switch (answers.action) {
+                    case 'View All Departments':
+                        await this.viewAllDepartments();
+                        break;
+                    case 'View All Roles':
+                        await this.viewAllRoles();
+                        break;
+                    case 'View All Employees':
+                        await this.viewAllEmployees();
+                        break;
+                    case 'Add a Department':
+                        await this.addDepartment(); // Await this so the prompt stays until the action is done
+                        break;
+                    case 'Add a Role':
+                        await this.addRole();
+                        break;
+                    case 'Add an Employee':
+                        await this.addEmployee();
+                        break;
+                    case 'Update an Employee Role':
+                        await this.updateEmployeeRole();
+                        break;
+                    case 'Exit':
+                        this.exit = true;
+                        process.exit(0);
+                        break;
+                    default:
+                        console.log('Invalid action');
+                }
+            
+                if (!this.exit) {
+                    await this.performActions(); // Wait for actions to complete before showing the menu again
+                }
+            }
+            
+    // async performActions(): Promise<void> {
+    // const answers = await inquirer.prompt([       
+    //     {
+    //       type: 'list',
+    //       name: 'action',
+    //       message: 'What Would You Like to Do?',
+    //       choices: [
+    //         'View All Departments',
+    //         'View All Roles',
+    //         'View All Employees',
+    //         'Add a Department',
+    //         'Add a Role',
+    //         'Add an Employee',
+    //         'Update an Employee Role',
+    //         'Exit',
+    //       ],
+    //     },
+    //   ])
+    //   .then((answers: any) => {
+    //     if (answers.action === 'View All Departments') {
+    //         this.viewAllDepartments();
+    //     } else if (answers.action === 'View All Roles') {
+    //          this.viewAllRoles();
+    //     }else if (answers.action === 'View All Employees') {
+    //          this.viewAllEmployees();
+    //     }else if (answers.action === 'Add a Department') {
+    //          this.addDepartment();
+    //     }else if (answers.action === 'Add a Role') {
+    //          this.addRole();
+    //     }else if (answers.action === 'Add an Employee') {
+    //          this.addEmployee();
+    //     }else if (answers.action === 'Update an Employee Role') {
+    //          this.updateEmployeeRole();
+    //     }else if (answers.action === 'Exit') {
+    //         this.exit = true;
+    //         process.exit(0);
+    //     }if (!this.exit) {
+    //         await this.performActions(); // Wait for actions to complete before showing the menu again
+    //     }
+    //   })
+    // }
 
     async viewAllDepartments(): Promise<any> {
         try {
@@ -83,10 +137,32 @@ class Queries {
         }
     }
 
-    async addDepartment() {
-        // Implement add department logic
+    async addDepartment(): Promise<void> {
+        try {
+            // Prompt the user for department input
+            const answers = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'department',
+                    message: 'Enter the department you would like to add:',
+                }
+            ]);
+    
+            const department = answers.department;
+    
+            // Perform the database insert operation
+            const result = await this.pool.query(`INSERT INTO department (name) VALUES ($1) RETURNING *;`, [department]);
+    
+            // Log the result to the console
+            console.log('Department added:');
+            console.table(result.rows); 
+    
+        } catch (err) {
+            console.error('Something went wrong while adding a department:', err);
+        } 
+       
     }
-
+    
     async addRole() {
         // Implement add role logic
     }
